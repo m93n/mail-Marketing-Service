e55de87c81cb0623f0bfbe5813fcf93e123a42a9
m93n <mohsen.k9731@gmail.com>
2024-12-10 16:11:01 +0330

    edit db setting to use postgresql (forgot set postgresql name istead of sqlite3

--- core/settings.py
+++ core/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
