c3529f1a1454aa6dd6ce69c83b76012282bf3330
m93n <mohsen.k9731@gmail.com>
2024-12-22 19:36:35 +0330

    add Subscription model

--- auth_service/models.py
+++ auth_service/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class CustomUser(AbstractUser):
     role = models.CharField(max_length=50, blank=True, null=True)  # Job Role
     company = models.CharField(max_length=255, blank=True, null=True)  # User's Company
+
+class Subscription(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    is_confirmed = models.BooleanField()
+    confirmation_code = models.CharField(max_length=250)
+    created_at = models.DateTimeField()
