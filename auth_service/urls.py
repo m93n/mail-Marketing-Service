4fc105292d091f16833249fc0828136c97d4fee5
m93n <mohsen.k9731@gmail.com>
2025-02-06 22:46:38 +0330

    update urls.py in auth_service

--- auth_service/urls.py
+++ auth_service/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
-from auth_service.views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, CustomTokenRefreshView
 
 urlpatterns = [
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]
