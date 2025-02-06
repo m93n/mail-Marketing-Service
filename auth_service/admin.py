994217e9097376b7c52682502ffd24973d439b9a
m93n <mohsen.k9731@gmail.com>
2025-02-06 19:22:13 +0330

    register SubscriptionAdmin in admin

--- auth_service/admin.py
+++ auth_service/admin.py
@@ -6,3 +6,11 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('email', 'full_name', 'role', 'is_active', 'is_staff')
     search_fields = ('email', 'full_name')
     ordering = ('email',)
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'start_date', 'end_date')
+    search_fields = ('user__email',)
+    ordering = ('-start_date',)
+
+
