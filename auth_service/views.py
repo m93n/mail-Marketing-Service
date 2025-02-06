391281a692f747d2643805acbf66389e5ef485cd
m93n <mohsen.k9731@gmail.com>
2025-02-06 22:07:41 +0330

    english docstring for custom token login endpoint

--- auth_service/views.py
+++ auth_service/views.py
@@ -2,8 +2,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer
 class CustomTokenObtainPairView(TokenObtainPairView):
     """
-    ویو ورود سفارشی برای کاربران معمولی با استفاده از JWT.
-    این ویو ایمیل و رمز عبور کاربر را دریافت می‌کند و در صورت موفقیت
-    توکن‌های access و refresh را برمی‌گرداند.
+    Custom login view for users by JWT.
+    This view takes email and password and if it was successful will
+    return access and refresh tokens.
     """
     serializer_class = CustomTokenObtainPairSerializer
