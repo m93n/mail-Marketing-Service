6a1e86283a82da6aa85b8ef68f0b64598235a6b7
m93n <mohsen.k9731@gmail.com>
2025-02-10 19:05:12 +0330

    create custom owner or readonly permission

--- auth_service/permissions.py
+++ auth_service/permissions.py
@@ -6,3 +6,13 @@ class IsAdminUser(BasePermission):
     """
     def has_permission(self, request, view):
         return request.user.role == 'admin'
+
+class IsOwnerOrReadOnly(BasePermission):
+    """
+    Access to data just for owner or read only ways. (GET, HEAD, OPTIONS).
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True  # permission to read data for all users
+        return obj.owner == request.user  # Only owner can edit
+
