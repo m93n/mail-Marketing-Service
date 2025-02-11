c30fa8937881190d87fec2d1ce8e58e4fa6443c5
m93n <mohsen.k9731@gmail.com>
2025-02-11 14:40:35 +0330

    create user serializer and subscription serializesr

--- auth_service/serializers.py
+++ auth_service/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from .models import CustomUser, Subscription
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     """
@@ -12,3 +14,17 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         data['email'] = self.user.email
         data['full_name'] = getattr(self.user, 'full_name', '')
         return data
+
+class UserSerializer(serializers.ModelSerializer):
+    role = serializers.CharField(read_only=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ["id", "full_name", "email", "role", "is_active"]
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)  # Displaying user information in JSON
+
+    class Meta:
+        model = Subscription
+        fields = "__all__"
