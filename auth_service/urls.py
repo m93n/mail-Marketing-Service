7e626ebe3e630f851dc430028e7bb1c5c7428a4c
m93n <mohsen.k9731@gmail.com>
2025-02-11 14:41:37 +0330

    add router url of user and subscription viewset

--- auth_service/urls.py
+++ auth_service/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
-from .views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, UserViewSet, SubscriptionViewSet
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'subscriptions', SubscriptionViewSet)
 
 urlpatterns = [
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('OAuth2/', include('allauth.urls')),
+    path('', include(router.urls)),
 ]
